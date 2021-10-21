a28d90c61d684ff2654ca7d11c03565ad56be3e7
Mellow Marshmallow <64708999+MellowMarshmallow@users.noreply.github.com>
2021-10-21 16:54:06 -0600

    Log JSON read and write

---
+++ src/pymon/io.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+
+"""
+For reading and writing data.
+"""
+
+
+from typing import Any
+import json
+import pymon.log
+
+
+logger = pymon.log.get_logger()
+
+
+def read(path: str) -> Any:
+    """Implements how to read data files."""
+
+    try:
+        logger.info("Read %s", path)
+
+        with open(path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        logger.info("Read %s ... done", path)
+        return data
+    except FileNotFoundError:
+        logger.critical("Read %s ... failed", path)
+        raise
+
+
+def write(path: str, serializable_data: Any) -> None:
+    """Implements how to write data files."""
+
+    try:
+        logger.info("Write data to %s", path)
+        logger.debug("data == %s", serializable_data)
+
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(serializable_data, file, indent=4, sort_keys=True)
+
+        logger.info("Write data to %s ... done", path)
+    except FileNotFoundError:
+        logger.critical("Unable to open %s", path)
+        raise
+    except TypeError:
+        logger.critical("Data is not serializable")
+        raise
